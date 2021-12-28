ad3acea97e74bd7957076d333ec4e0c5f50563b3
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2021-12-28 17:24:44 -0300

    Colocando as coisas pra quando a api tiver pronta

--- src/contexts/DrinkContext.tsx
+++ src/contexts/DrinkContext.tsx
@@ -2,12 +2,12 @@ import { createContext, useState, ReactNode, useContext, useEffect } from "react
 import DrinkModel from "../model/Drink";
 import UserModel from "../model/User";
 
+import axios from 'axios'
+
 
 type DrinkContextData = {
     lstUsers: UserModel[];
     setLstUsers:React.Dispatch<React.SetStateAction<Array<UserModel>>>;
-    lstDrinks: DrinkModel[];
-    setLstDrinks: React.Dispatch<React.SetStateAction<Array<DrinkModel>>>;
 }
 
 
@@ -19,12 +19,39 @@ type DrinkContextProviderProps = {
 
 export function DrinkContextProvider({children}:DrinkContextProviderProps){
     const [lstUsers,setLstUsers] = useState<Array<UserModel>>([])
-    const [lstDrinks,setLstDrinks] = useState<Array<DrinkModel>>([])
 
-    
+    const baseUrl = `3.142.54.6/user/`
+    useEffect(()=>{
+        //base url 3.142.54.6
+        getUserData()
+    },[])
+
+    async function getUserData() {
+        const userData:any = await axios.get(baseUrl).then(resp=>resp.data)
+        let lstUserAux:Array<UserModel> = []
+
+        userData.map(async(user:any)=>{
+            const userDrinks = await getDrinkLst(user.Id)
+            const newUser = new UserModel(user.Id,user.Name,user.Path,userDrinks)
+            lstUserAux.push(newUser)
+        })
+
+        setLstUsers(lstUserAux)
+    }
+
+    async function getDrinkLst(idUser:number){
+        const drinkData:any = await axios.get(`${baseUrl}${idUser}/drinks`).then(resp=>resp.data)
+
+        let lstDrinkAux:Array<DrinkModel> = []
+        drinkData.map((drink:any)=>{
+            const newDrink = new DrinkModel(drink.Id,idUser,drink.Name,drink.None)
+            lstDrinkAux.push(newDrink)
+        })
+        return lstDrinkAux
+    }
 
     return <DrinkContext.Provider value={{
-        lstUsers, lstDrinks,setLstUsers,setLstDrinks
+        lstUsers,setLstUsers
     }}>
         {children}
     </DrinkContext.Provider>
--- src/model/User.ts
+++ src/model/User.ts
@@ -1,11 +1,15 @@
+import DrinkModel from "./Drink";
+
 export default class UserModel{
     id:number 
     name: string
     path:string
+    lstDrinks:DrinkModel[]
 
-    constructor(id:number,name:string,path:string){
+    constructor(id:number,name:string,path:string,lstDrinks:DrinkModel[]){
         this.id = id;
         this.name = name
         this.path = path
+        this.lstDrinks = lstDrinks
     }    
 }
